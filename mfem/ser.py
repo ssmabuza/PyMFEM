ecfbab5bb45ffcfed42838d81a4d6acffd081ea4
S. Shiraiwa <shiraiwa@princeton.edu>
2025-12-14 12:10:32 -0500

    included Python3.14 and added ordering in ser.py and par.py

--- mfem/ser.py
+++ mfem/ser.py
@@ -61,6 +61,7 @@ from  mfem._ser.qfunction import *
 from  mfem._ser.quadinterpolator import *
 from  mfem._ser.quadinterpolator_face import *
 from  mfem._ser.attribute_sets import *
+from  mfem._ser.ordering import *
 
 from  mfem._ser.fe_base import *
 from  mfem._ser.fe_h1 import *
